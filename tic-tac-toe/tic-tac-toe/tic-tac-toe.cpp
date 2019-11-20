990d41d6b162968b1e644a674937d1c6b37cbf6a
hdduy110697 <hdduy110697@gmail.com>
2019-11-20 15:22:54 +0700

    add CheckWinHorizontal and CheckWinCollump

--- tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
+++ tic-tac-toe/tic-tac-toe/tic-tac-toe.cpp
@@ -3,7 +3,11 @@
 
 #include <iostream>
 #include <string>
+#include "tic-tac-toe.h"
 using namespace std;
+
+#define SIZE_BOARD 3
+#define MARK_WIN 3
 
 void ShowTheBoard(string board[3][3])
 {
@@ -16,62 +20,155 @@ void ShowTheBoard(string board[3][3])
 }
 
 //mark turn player1 (mark X)
-void PlayChessPlayer1(string location, string board[3][3]) {
+void PlayChessPlayer(string location, string board[SIZE_BOARD][SIZE_BOARD], int Player) {
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 			if (location.compare(board[i][j]) == 0) {
-				board[i][j] = "X";
+				if (Player == 1)
+				{
+					board[i][j] = "X";
+				}
+				else
+				{
+					board[i][j] = "O";
+				}
+
 			}
 	}
 }
-//mark turn player2 (mark Y)
-void PlayChessPlayer2(string location, string board[3][3]) {
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-			if (location.compare(board[i][j]) == 0) {
-				board[i][j] = "O";
-			}
-	}
-}
+
+
 // win return -1 ; draw return 0
-int checkwin(string board[3][3]) {
-	// check win in a row
-	if (board[0][0] == board[0][1] && board[0][1] == board[0][2])
+//int CheckWin(string board[SIZE_BOARD][SIZE_BOARD]) {
+//	// check win in a row
+//	if (board[0][0] == board[0][1] && board[0][1] == board[0][2])
+//
+//		return 1;
+//	else if (board[1][0] == board[1][1] && board[1][1] == board[1][2])
+//
+//		return 1;
+//	else if (board[2][0] == board[2][1] && board[2][1] == board[2][2])
+//
+//		return 1;//true
+//	// check win in a collump
+//	else if (board[0][0] == board[1][0] && board[1][0] == board[2][0])
+//
+//		return 1;
+//	else if (board[0][1] == board[1][1] && board[1][1] == board[2][1])
+//
+//		return 1;
+//	else if (board[0][2] == board[1][2] && board[1][2] == board[2][2])
+//
+//		return 1;//true
+//	//check win in a diagonal
+//	else if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+//
+//		return 1;
+//	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+//
+//		return 1;
+//	//check draw
+//	else if (board[0][0] != "11" && board[0][1] != "12" && board[0][2] != "13"
+//		&& board[1][0] != "21" && board[1][1] != "22" && board[1][2] != "23"
+//		&& board[2][0] != "31" && board[2][1] != "32" && board[2][2] != "33")
+//		return 0;
+//	else
+//		return -1;
+//	return 0;
+//}
+bool CheckWinhorizontal(string location, string board[SIZE_BOARD][SIZE_BOARD], int player) {
+	string mark = "";
 
-		return 1;
-	else if (board[1][0] == board[1][1] && board[1][1] == board[1][2])
-
-		return 1;
-	else if (board[2][0] == board[2][1] && board[2][1] == board[2][2])
-
-		return 1;//true
-	// check win in a collump
-	else if (board[0][0] == board[1][0] && board[1][0] == board[2][0])
-
-		return 1;
-	else if (board[0][1] == board[1][1] && board[1][1] == board[2][1])
-
-		return 1;
-	else if (board[0][2] == board[1][2] && board[1][2] == board[2][2])
-
-		return 1;//true
-	//check win in a diagonal
-	else if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
-
-		return 1;
-	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
-
-		return 1;
-	//check draw
-	else if (board[0][0] != "11" && board[0][1] != "12"  && board[0][2] != "13"
-		&& board[1][0] != "21" && board[1][1] != "22" && board[1][2] != "23"
-		&& board[2][0] != "31" && board[2][1] != "32" && board[2][2] != "33")
-		return 0;
+	if (player == 1)
+	{
+		mark = "X";
+	}
 	else
-		return -1;
-	return 0;
+	{
+		mark = "Y";
+	}
+	int posX = (int)location[0] - '1';
+	int posY = (int)location[1] - '1';
+	int countLeft = 0;
+	int countRight = 0;
+	//count left
+	for (int i = posX; i >= 0; i--)
+	{
+		if (board[posY][i] == mark)
+		{
+			countLeft++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	//count Right
+	for (int i = posX + 1; i < SIZE_BOARD; i++)
+	{
+		if (board[posY][i] == mark)
+		{
+			countRight++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	bool result = countLeft + countRight == MARK_WIN;
+	return result;
+}
+bool CheckWinColumn(string location, string board[SIZE_BOARD][SIZE_BOARD], int player) {
+	string mark = "";
+
+	if (player == 1)
+	{
+		mark = "X";
+	}
+	else
+	{
+		mark = "Y";
+	}
+	int posY = (int)location[0] - '1';
+	cout << posY << endl;
+	int posX = (int)location[1] - '1';
+	cout << posX << endl;
+	int countUp = 0;
+	int countDown = 0;
+	for (int i = posY; i >= 0; i--)
+	{
+		if (board[i][posX] == mark)
+		{
+			countUp++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	for (int i = posY + 1; i < SIZE_BOARD; i++)
+	{
+
+		if (board[i][posX] == mark)
+		{
+			countDown++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return countUp + countDown == MARK_WIN;
+}
+
+int CheckWin(string board[SIZE_BOARD][SIZE_BOARD], string location, int player) {
+	cout << "ham check in:" << location << endl;
+	if (CheckWinhorizontal(location, board, player) == true || CheckWinColumn(location, board, player) == true)
+	{
+		return 1;
+	}
+	return -1;
 }
 int main()
 {
@@ -85,7 +182,7 @@ int main()
 	cin >> mode;
 	string namePlayer1;
 	string namePlayer2;
-	
+
 	if (mode == 1) {
 		cout << "Enter Player 1 Name:";
 		cin >> namePlayer1;
@@ -98,54 +195,57 @@ int main()
 	}
 
 	string chess = "";
-	string  board[3][3] = { {"11","12","13"},{"21","22","23"},{"31","32","33"} };
+	string  board[SIZE_BOARD][SIZE_BOARD] = { {"11","12","13"},{"21","22","23"},{"31","32","33"} };
 	ShowTheBoard(board);
 	do
 	{
-		
+
 		player = (player % 2) ? 1 : 2;
-		if (player == 1) {			
+		if (player == 1) {
 			do {
 				cout << "Your Turn  " << namePlayer1 << ":";
 				cin >> chess;
-			} while (board[(int)chess[0]-'1'][(int)chess[1]-'1'] == "X" || board[(int)chess[0] - '1'][(int)chess[1] - '1'] == "O"
-				|| (int)chess[0] - '1'<0 || (int)chess[0] - '1' >=3 || (int)chess[1] - '1' <0 || (int)chess[1] - '1' >=3);
-			
-			PlayChessPlayer1(chess, board);
+			} while (board[(int)chess[0] - '1'][(int)chess[1] - '1'] == "X" || board[(int)chess[0] - '1'][(int)chess[1] - '1'] == "O"
+				|| (int)chess[0] - '1' < 0 || (int)chess[0] - '1' >= 3 || (int)chess[1] - '1' < 0 || (int)chess[1] - '1' >= 3);
+
+			PlayChessPlayer(chess, board, player);
 			ShowTheBoard(board);
+			checkWin = CheckWin(board, chess, player);
 			player++;
-			checkWin = checkwin(board);
-			if (checkWin == 1 || checkWin ==0) break;
+			cout << checkWin;
+			if (checkWin == 1 || checkWin == 0) break;
 		}
 		if (player == 2)
 		{
-			
+
 			do {
 				cout << "Your Turn  " << namePlayer2 << ":";
 				cin >> chess;
 			} while (board[(int)chess[0] - '1'][(int)chess[1] - '1'] == "X" || board[(int)chess[0] - '1'][(int)chess[1] - '1'] == "O"
 				|| (int)chess[0] - '1' < 0 || (int)chess[0] - '1' >= 3 || (int)chess[1] - '1' < 0 || (int)chess[1] - '1' >= 3);
-			PlayChessPlayer2(chess, board);
+			PlayChessPlayer(chess, board, player);
+			checkWin = CheckWin(board, chess, player);
+			cout << checkWin;
 			ShowTheBoard(board);
 			player--;
 		}
-		checkWin = checkwin(board);
 	} while (checkWin == -1);
-	if (checkWin ==1)
+	//show player win
+	if (checkWin == 1)
 	{
 		if (player == 1) {
-			cout << "player "<<namePlayer2 <<" win";
+			cout << "player " << namePlayer2 << " win";
 		}
 		else
 		{
 			cout << "player " << namePlayer1 << " win";
 		}
 	}
-	if (checkWin==0)
+	if (checkWin == 0)
 	{
 		cout << "draw";
 	}
-		
-	
+
+
 }
 
